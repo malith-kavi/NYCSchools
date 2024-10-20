446f836d66f1cb4431bacc2d1560ef1d9733e596
Malith Kavinda <138204165+malith-kavi@users.noreply.github.com>
2024-10-20 23:08:24 +0530

    Update SchoolDirectory.swift

--- NYCSchools/App/Data Layer/Networking/SchoolDirectory.swift
+++ NYCSchools/App/Data Layer/Networking/SchoolDirectory.swift
@@ -17,8 +17,51 @@ class SchoolAPI: SchoolAPILogic{
     }
         
     func getSchools() {
-        //ToDo: impliment me
-        print(Constants.schoolListURL)
-        print("impliment get School")
+        AF.request(Constants.schoolListURL)
+            .validate()
+            .responseDecodable(of: [School].self) { result in
+                switch result {
+                case .failure(let error):
+                    break
+                case .success(let school):
+                    break
+                }
+            }
+    }
+    
+    private func retrieveSchoolUsingStandardServices(){
+        var urlComponents = URLComponents()
+        urlComponents.scheme = "https"
+        urlComponents.host = "data.cityofnewyork.us"
+        urlComponents.path = "/resource/s3k6-pzi2.json"
+        urlComponents.queryItems = [URLQueryItem(name: "$$app_token", value: "L1KwLSwm1yz1N7aWqFCF4dLmM")]
+        
+        //another way to get URL
+        //URL(string: schoolListURL)
+        
+        if let url = urlComponents.url{
+            let urlSession = URLSession(configuration: .default)
+            
+            let task = urlSession.dataTask(with: url) { data, response, error in
+                guard error == nil else {
+                    print("error occured \(error)")
+                    return
+            }
+                
+            if let data = data {
+                let decoder = JSONDecoder()
+                do {
+                    let schools = try decoder.decode([School].self,
+                                                     from: data)
+                    print("schools\(schools)")
+                } catch let error {
+                    print("error during passing\(error)")
+                }
+                
+                    
+                }
+            }
+            task.resume()
+        }
     }
 }
