393e3a1f99f7108d4d534adff325d33148e3eb15
Malith Kavinda <138204165+malith-kavi@users.noreply.github.com>
2024-10-22 01:23:16 +0530

    Create SchoolListLoadState.swift

---
+++ NYCSchools/App/Data Layer/Models/SchoolListLoadState.swift
@@ -0,0 +1,14 @@
+//
+//  SchoolListState.swift
+//  NYCSchools
+//
+//  Created by Malith Kavinda on 2024-10-22.
+//
+
+import Foundation
+
+enum SchoolListLoadState {
+    case error
+    case loaded
+    case empty
+}
