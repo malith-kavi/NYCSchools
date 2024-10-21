f3a605e74daa0c3b8a433b025758d15414b87c61
Malith Kavinda <138204165+malith-kavi@users.noreply.github.com>
2024-10-22 00:29:33 +0530

    Update ViewController.swift

--- NYCSchools/App/UI Layer/View Controllers/ViewController.swift
+++ NYCSchools/App/UI Layer/View Controllers/ViewController.swift
@@ -8,23 +8,26 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private let schoolViewModel: SchoolsViewModel = SchoolsViewModel()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        schoolViewModel.getSchools()
         
-        let api: SchoolAPILogic = SchoolAPI()
-        api.getSchools { result in
-            switch result {
-            case .failure(let error):
-                print("error retrieving schools: \(error.localizedDescription)")
-            case .success(let schools):
-                if let schools = schools {
-                    print("found sschools \(schools.count)")
-                }
-            }
-        }
-    }
+//        let api: SchoolAPILogic = SchoolAPI()
+//        api.getSchools { result in
+//            switch result {
+//            case .failure(let error):
+//                print("error retrieving schools: \(error.localizedDescription)")
+//            case .success(let schools):
+//                if let schools = schools {
+//                    print("found sschools \(schools.count)")
+//                }
+//            }
+//        }
+   }
 
 
 }
