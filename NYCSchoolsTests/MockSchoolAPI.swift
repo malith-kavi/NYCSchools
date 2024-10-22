940c20ec1f5683abe74049be8c36b174f504c3d4
Malith Kavinda <138204165+malith-kavi@users.noreply.github.com>
2024-10-23 01:07:41 +0530

    Update MockSchoolAPI.swift

--- NYCSchoolsTests/MockSchoolAPI.swift
+++ NYCSchoolsTests/MockSchoolAPI.swift
@@ -11,29 +11,29 @@ import Foundation
 class MockSchoolAPI: SchoolAPILogic {
     var loadState: SchoolListLoadState = .empty
     
-    func getSchools(completion: @escaping (SchoolListAPIResponse)) {
+    func getSchools(completion: @escaping (NYCSchools.SchoolListAPIResponse)) {
         switch loadState {
         case .error:
-            completion( .failure(.networkingError("Could not fetch schools")))
+            completion(.failure(.networkingError("Could not fetch schools")))
         case .loaded:
-            let mock = School(dbn: <#T##String#>,
-                              schoolName: <#T##String?#>,
-                              overviewParagraph: <#T##String?#>,
-                              schoolEmail: <#T##String?#>,
-                              academicOpportunities1: <#T##String?#>,
-                              academicOpportunities2: <#T##String?#>,
-                              neighborhood: <#T##String?#>,
-                              phoneNumber: <#T##String?#>,
-                              website: <#T##String?#>,
-                              finalgrades: <#T##String?#>,
-                              totalStudents: <#T##String?#>,
-                              schoolSports: <#T##String?#>,
-                              primaryAddressLine: <#T##String?#>,
-                              city: <#T##String?#>,
-                              zip: <#T##String?#>,
-                              latitude: <#T##String?#>,
-                              longitude: <#T##String?#>)
-            completion(.success([mock]))
+            let mockSchool = School(dbn: "02M260",
+                                    schoolName: "Clinton School Writers & Artists, M.S. 260",
+                                    overviewParagraph: "Students who are prepared for college must have an education that encourages them to take...",
+                                    schoolEmail: "admissions@theclintonschool.net",
+                                    academicOpportunities1: "Free college courses at neighboring universities",
+                                    academicOpportunities2: "International Travel, Special Arts Programs, Music, Internships, College Mentoring English Language Learner Programs: English as a New Language",
+                                    neighborhood: "Chelsea-Union Sq",
+                                    phoneNumber: "212-524-4360",
+                                    website: "www.theclintonschool.net",
+                                    finalGrades: "6-12",
+                                    totalStudents: "376",
+                                    schoolSports: "Cross Country, Track and Field, Soccer, Flag Football, Basketball",
+                                    primaryAddressLine: "10 East 15th Street",
+                                    city: "Manhattan",
+                                    zip: "10003",
+                                    latitude: "40.73653",
+                                    longitude: "-73.9927")
+            completion(.success([mockSchool]))
         case .empty:
             completion(.success([]))
         }
