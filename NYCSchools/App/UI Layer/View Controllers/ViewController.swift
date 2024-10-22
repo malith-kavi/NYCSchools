fd028d42dd61b13b91640249390d9e1c8ee57373
Malith Kavinda <138204165+malith-kavi@users.noreply.github.com>
2024-10-23 01:07:24 +0530

    Update ViewController.swift

--- NYCSchools/App/UI Layer/View Controllers/ViewController.swift
+++ NYCSchools/App/UI Layer/View Controllers/ViewController.swift
@@ -6,29 +6,44 @@
 //
 
 import UIKit
+import Combine
 
 class ViewController: UIViewController {
     
-    private let schoolViewModel: SchoolsViewModel = SchoolsViewModel()
+    private let schoolsViewModel: SchoolsViewModel = SchoolsViewModel()
+    private var cancellables = Set<AnyCancellable>()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        schoolViewModel.getSchools { [weak self] (schools, error) in
-            if let error = error {
-                let alert = UIAlertController(title: "Error", message: "Could not be retrive schools: \(error.localizedDescription)", preferredStyle: .alert)
-                let action = UIAlertAction(title: "OK", style: .default)
-                alert.addAction(action)
-                self?.present(alert, animated: true)
+        setupBinders()
+        schoolsViewModel.getSchools()
+        
+        let titleStr = "schools.title".localized()
+        printContent("\(titleStr)")
+    }
+    
+    private func setupBinders() {
+        schoolsViewModel.$schools
+            .receive(on: RunLoop.main)
+            .sink { schools in
+                    print("retrived \(schools.count) schools")
             }
-            
-            if let schools = schools {
-                print("retrived \(schools.count) schools")
+            .store(in: &cancellables)
+        
+        schoolsViewModel.$error
+            .receive(on: RunLoop.main)
+            .sink { [weak self] error in
+                if let error = error {
+                    let alert = UIAlertController(title: "Error", message: "Could not be retrive schools: \(error.localizedDescription)", preferredStyle: .alert)
+                    let action = UIAlertAction(title: "OK", style: .default)
+                    alert.addAction(action)
+                    self?.present(alert, animated: true)
+                }
             }
-        }
-
-   }
-
-
+            .store(in: &cancellables)
+    }
 }
+
+
+
 
