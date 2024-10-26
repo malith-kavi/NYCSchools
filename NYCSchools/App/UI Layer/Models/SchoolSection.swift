4cbfc1370dda0b509c536fc05a750aef92e1e60b
Malith Kavinda <138204165+malith-kavi@users.noreply.github.com>
2024-10-26 23:37:33 +0530

    Create SchoolSection.swift

---
+++ NYCSchools/App/UI Layer/Models/SchoolSection.swift
@@ -0,0 +1,13 @@
+//
+//  SchoolSection.swift
+//  NYCSchools
+//
+//  Created by Malith Kavinda on 2024-10-26.
+//
+
+import Foundation
+
+struct SchoolSection {
+    let city: String
+    var schools: [School]
+}
