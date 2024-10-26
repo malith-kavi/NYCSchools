b6b91d9c11845aae940153802220685db9529085
Malith Kavinda <138204165+malith-kavi@users.noreply.github.com>
2024-10-26 23:37:45 +0530

    Update SchoolListCollectionViewController.swift

--- NYCSchools/App/UI Layer/View Controllers/SchoolListCollectionViewController.swift
+++ NYCSchools/App/UI Layer/View Controllers/SchoolListCollectionViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 import Combine
 import PureLayout
 
-class SchoolListCollectionViewController: UIViewController {
+class SchoolsCollectionViewController: UIViewController {
     
     private let schoolsViewModel: SchoolsViewModel = SchoolsViewModel()
     private var cancellables = Set<AnyCancellable>()
@@ -19,36 +19,43 @@ class SchoolListCollectionViewController: UIViewController {
         static let cellIdentifier: String = "schoolCell"
         static let cellHeight: CGFloat = 100
         static let sectionHeaderIdentifier: String = "sectionHeader"
-        static let sectionHeight:CGFloat = 50
+        static let sectionHeight: CGFloat = 50
     }
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setupCollectionView()
         setupBinders()
         schoolsViewModel.getSchools()
         
-        let titleStr = "schools.title".localized()
-        printContent("\(titleStr)")
+        title = "schools.list.nav.title".localized()
     }
     
     private func setupCollectionView() {
         let collectionViewLayout = UICollectionViewFlowLayout()
-        collectionViewLayout.itemSize = CGSize(width: view.frame.size.width, height: Constants.cellHeight)
-// 
+        collectionViewLayout.itemSize = CGSize(width: view.frame.size.width,
+                                               height: Constants.cellHeight)
+        collectionViewLayout.headerReferenceSize = CGSize(width: view.frame.size.width,
+                                                          height: Constants.sectionHeight)
         collectionViewLayout.scrollDirection = .vertical
-        collectionView = UICollectionView(frame: view.frame, collectionViewLayout: collectionViewLayout
-        )
+        collectionView = UICollectionView(frame: view.frame,
+                                          collectionViewLayout: collectionViewLayout)
+        
         guard let collectionView = collectionView else {
             return
         }
+        
         view.addSubview(collectionView)
         collectionView.autoPinEdgesToSuperviewEdges()
         collectionView.backgroundColor = .white
         collectionView.alwaysBounceVertical = true
         
         //setup & customize flow layout
-        collectionView.register(SchoolCollectionViewCell.self, forCellWithReuseIdentifier: Constants.cellIdentifier)
+        collectionView.register(SchoolCollectionViewCell.self,
+                                forCellWithReuseIdentifier: Constants.cellIdentifier)
+        collectionView.register(SchoolSectionHeaderView.self,
+                                forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader,
+                                withReuseIdentifier: Constants.sectionHeaderIdentifier)
         collectionView.dataSource = self
         collectionView.delegate = self
     }
@@ -57,11 +64,10 @@ class SchoolListCollectionViewController: UIViewController {
         schoolsViewModel.$schools
             .receive(on: RunLoop.main)
             .sink { [weak self] schools in
-                    if !schools.isEmpty {
-                    print("retrived \(schools.count) schools")
+                if !schools.isEmpty {
+                    print("retrieved \(schools.count) schools")
                     self?.collectionView?.reloadData()
                 }
-                    
             }
             .store(in: &cancellables)
         
@@ -69,38 +75,61 @@ class SchoolListCollectionViewController: UIViewController {
             .receive(on: RunLoop.main)
             .sink { [weak self] error in
                 if let error = error {
-                    let alert = UIAlertController(title: "Error", message: "Could not be retrive schools: \(error.localizedDescription)", preferredStyle: .alert)
-                    let action = UIAlertAction(title: "OK", style: .default)
+                    let alert = UIAlertController(title: "Error",
+                                                  message: "Could not retrieve schools: \(error.localizedDescription)",
+                                                  preferredStyle: .alert)
+                    
+                    let action = UIAlertAction(title: "OK",
+                                               style: .default)
                     alert.addAction(action)
-                    self?.present(alert, animated: true)
+                    self?.present(alert,
+                                  animated: true)
                 }
             }
             .store(in: &cancellables)
     }
 }
 
-extension SchoolListCollectionViewController: UICollectionViewDataSource {
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return schoolsViewModel.schools.count
+/// Implementation of data source delegate for collection view to help with displaying schools
+extension SchoolsCollectionViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView,
+                        numberOfItemsInSection section: Int) -> Int {
+        return schoolsViewModel.schoolSectionsList?[section].schools.count ?? 0
     }
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: Constants.cellIdentifier,
                                                             for: indexPath) as? SchoolCollectionViewCell else {
             return UICollectionViewCell()
         }
-        
-        if let schoolSection = schoolsViewModel.schoolSectionsList[indexPath.section]
-        let school = schoolsViewModel.schools[indexPath.item]
-        cell.populate(school)
+        if let schoolSection = schoolsViewModel.schoolSectionsList?[indexPath.section] {
+            let school = schoolSection.schools[indexPath.item]
+            cell.populate(school)
+        }
         return cell
     }
     
     func numberOfSections(in collectionView: UICollectionView) -> Int {
-        return 1
+        return schoolsViewModel.schoolSectionsList?.count ?? 1
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        viewForSupplementaryElementOfKind kind: String,
+                        at indexPath: IndexPath) -> UICollectionReusableView {
+        
+        if kind == UICollectionView.elementKindSectionHeader,
+           let sectionHeader = collectionView.dequeueReusableSupplementaryView(ofKind: kind,
+                                                                               withReuseIdentifier: Constants.sectionHeaderIdentifier,
+                                                                               for: indexPath) as? SchoolSectionHeaderView  {
+            sectionHeader.headerLabel.text = schoolsViewModel.schoolSectionsList?[indexPath.section].city
+            return sectionHeader
+        }
+        return UICollectionReusableView()
     }
 }
 
-extension SchoolListCollectionViewController: UICollectionViewDelegate {
+extension SchoolsCollectionViewController: UICollectionViewDelegate {
     
 }
-
